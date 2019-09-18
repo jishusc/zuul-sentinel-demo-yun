2c7ed48c0237559cd5d4b32c97676a245e3eb064
jishusc <jishusc@163.com>
2019-09-18 11:23:52 +0800

    zuul配置路由规则；对于jdk1.7+spring-cloud-alibaba 1.5版本，pom以及application.properites里修改一下

--- src/main/java/com/example/demo/ZuulSentinelDemoYunApplication.java
+++ src/main/java/com/example/demo/ZuulSentinelDemoYunApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulSentinelDemoYunApplication {
 
 	public static void main(String[] args) {
